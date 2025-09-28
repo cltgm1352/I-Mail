efe1a5f0dde20120230613161fc6cfa74926f8a7
HARUOMILABO <haruominaito1@gmail.com>
2025-09-28 11:34:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// ローディング → メイン画面へ
 window.addEventListener("load", () => {
+  // 1. ローディング終了 → メイン画面表示
   setTimeout(() => {
-    document.getElementById("loading-screen").style.display = "none";
-    document.getElementById("main-content").style.display = "block";
-  }, 2000); // 2秒でローディング終了
-});
-
-// 偽リンククリック → 警告画面へ
-document.addEventListener("DOMContentLoaded", () => {
-  const fakeLink = document.getElementById("fake-link");
-  fakeLink.addEventListener("click", (e) => {
-    e.preventDefault();
-    document.getElementById("main-content").style.display = "none";
-    document.getElementById("alert-screen").style.display = "flex";
-  });
-});
+    document.getElementById("loading-screen").style.display =
